a63413260a1b610f4906ead7f521af65a9a8cc01
Michael Chang <thenewme91@gmail.com>
2013-05-11 02:16:03 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walrus::Application.config.secret_token = '6ef16804bebb942c7b2b8308d44df17216b5fa8fe3f5a3bbde8313c4ea82246e83f63328633c623b8f49622f3dcb3308509eb16df07f1e02e9e60a66491102f4'
